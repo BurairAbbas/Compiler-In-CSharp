575d8b985004822e96249df2d94f635a48f4be39
BurairAbbas <65166629+BurairAbbas@users.noreply.github.com>
2021-03-07 15:57:07 +0500

    Update Design for Syntax Analyzer

--- CC Compiler In WFA/Form1.Designer.cs
+++ CC Compiler In WFA/Form1.Designer.cs
@@ -31,6 +31,7 @@
             this.Codetb = new System.Windows.Forms.TextBox();
             this.GenerateToken = new System.Windows.Forms.Button();
             this.tokentb = new System.Windows.Forms.TextBox();
+            this.Syntaxtb = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // Codetb
@@ -39,34 +40,44 @@
             this.Codetb.Location = new System.Drawing.Point(30, 12);
             this.Codetb.Multiline = true;
             this.Codetb.Name = "Codetb";
-            this.Codetb.Size = new System.Drawing.Size(401, 426);
+            this.Codetb.Size = new System.Drawing.Size(435, 267);
             this.Codetb.TabIndex = 0;
             // 
             // GenerateToken
             // 
-            this.GenerateToken.Location = new System.Drawing.Point(477, 189);
+            this.GenerateToken.Location = new System.Drawing.Point(550, 183);
             this.GenerateToken.Name = "GenerateToken";
             this.GenerateToken.Size = new System.Drawing.Size(140, 51);
             this.GenerateToken.TabIndex = 1;
-            this.GenerateToken.Text = "Generate Token";
+            this.GenerateToken.Text = "Compile";
             this.GenerateToken.UseVisualStyleBackColor = true;
             this.GenerateToken.Click += new System.EventHandler(this.GenerateToken_Click);
             // 
             // tokentb
             // 
             this.tokentb.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.tokentb.Location = new System.Drawing.Point(659, 12);
+            this.tokentb.Location = new System.Drawing.Point(776, 12);
             this.tokentb.Multiline = true;
             this.tokentb.Name = "tokentb";
             this.tokentb.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.tokentb.Size = new System.Drawing.Size(403, 426);
+            this.tokentb.Size = new System.Drawing.Size(396, 426);
             this.tokentb.TabIndex = 2;
+            // 
+            // Syntaxtb
+            // 
+            this.Syntaxtb.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Syntaxtb.Location = new System.Drawing.Point(30, 313);
+            this.Syntaxtb.Multiline = true;
+            this.Syntaxtb.Name = "Syntaxtb";
+            this.Syntaxtb.Size = new System.Drawing.Size(435, 125);
+            this.Syntaxtb.TabIndex = 3;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1074, 450);
+            this.ClientSize = new System.Drawing.Size(1184, 450);
+            this.Controls.Add(this.Syntaxtb);
             this.Controls.Add(this.tokentb);
             this.Controls.Add(this.GenerateToken);
             this.Controls.Add(this.Codetb);
@@ -82,6 +93,7 @@
         private System.Windows.Forms.TextBox Codetb;
         private System.Windows.Forms.Button GenerateToken;
         private System.Windows.Forms.TextBox tokentb;
+        private System.Windows.Forms.TextBox Syntaxtb;
     }
 }
 
